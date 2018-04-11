d9b2235ec24dbddf5592339df408509b50769ac8
prisilamichelle <prisilamichelle@yahoo.com>
2018-04-11 22:28:50 +0700

    added

--- src/Guppy/Guppy.cpp
+++ src/Guppy/Guppy.cpp
@@ -2,10 +2,11 @@
 #include <cmath>
 #include <time.h>
 #include <iostream>
+using namespace std;
 
-#define LAPAR 100
+#define LAPAR 200
 #define KOIN 1
-#define MATI 100
+#define MATI 2000
 #define NGEGEDEIN 5
 
 double fRand(double fMin, double fMax)
@@ -80,6 +81,7 @@ void Guppy::setLastCoin(int lc){
 
 //Service
 void Guppy::tick(LinkedList<Food> &foods, LinkedList<Coin> &coins, double delay){
+	bool cek = false;
 	/*if (((this->getTickTime())-lastCoin)>=KOIN){
 		lastCoin=(this->getTickTime());
 		if ((this->G)==1){
@@ -97,111 +99,75 @@ void Guppy::tick(LinkedList<Food> &foods, LinkedList<Coin> &coins, double delay)
 		}
 	}*/
 	
-	/*if (this->hunger){
-		if ((this->lastFed)>=LAPAR+MATI){
+	if (this->hunger){
+		if ((this->tickTime - this->lastFed)>=LAPAR+MATI){
 			this->getSpace()->remove(this->getId(), TYPE_GUPPY);
+			cek = true;
 		}else{
 			Food *food = findNearestFood(foods);
-			if (this->isAbleToConsume(*food)){
-				this->getSpace()->remove(food->getId(), TYPE_FOOD);
-				setLastFed(this->tickTime);
-				setHunger(false);
-				growthCounter++;
-				if (((this->tickTime)-(this->lastDrift))>=this->driftLength){
-					setDirection(fRand(0,8*atan(1)));	//randomize direction
-					driftLength = rand() % 3;	//maksimal 7 tick
-					lastDrift = this->tickTime;
-				}
 
-				if ((getDirection()>=2*atan(1))&&(getDirection()<=6*atan(1))){
-					setDirRight(true);
-				}else{
-					setDirRight(false);
-				}
-
-				if ((this->dirRight)&&((this->G)==1)){	//menentukan gambar ikan yang dipakai
-					this->setImage("small-guppy-right.png");
-				}
-				else if ((this->dirRight)&&((this->G)==2)){
-					this->setImage("medium-guppy-right.png");
-				}
-				else if ((this->dirRight)&&((this->G)==3)){
-					this->setImage("large-guppy-right.png");
-				}
-				else if ((!this->dirRight)&&((this->G)==1)){
-					this->setImage("small-guppy-left.png");
-				}
-				else if ((!this->dirRight)&&((this->G)==2)){
-					this->setImage("medium-guppy-left.png");
-				}
-				else if ((!this->dirRight)&&((this->G)==3)){
-					this->setImage("large-guppy-left.png");
-				}
-
-				//Ngejalanin ikannya, berdasarkan dir yang udah pasti bener:
-				double newX = this->getX() + 100*cos(this->getDirection())*delay; //speed bisa diatur2 lah ntar
-				double newY = this->getY() + 100*sin(this->getDirection())*delay;
-				if (this->getSpace()->moveTo(this->getId(), TYPE_GUPPY, newX, newY)) {
-					this->setX(newX);
-					this->setY(newY);
-
-				//Update atribut:
-					if ((this->lastFed)>=LAPAR){
-						setHunger(true);
-					}
-				}else{
-					setDirection(getDirection()+2*atan(1));
-				}
-			}else{
-				//ngejar food, belum implementasi (findNearestFood, setDir, setSpeed)
-				if (food != NULL){
-					setDirection(atan2(food->getY()-this->getY(), food->getX()-this->getX()));
-					if ((getDirection()>=2*atan(1))&&(getDirection()<=6*atan(1))){
-						setDirRight(true);
-					}
-					else {
-						setDirRight(false);
-					}
-
-					if ((this->dirRight)&&((this->G)==3)){	//menentukan gambar ikan yang dipakai
-						this->setImage("small-guppy-right.png");
-					}
-					else if ((this->dirRight)&&((this->G)==2)){
-						this->setImage("medium-guppy-right.png");
-					}
-					else if ((this->dirRight)&&((this->G)==1)){
-						this->setImage("large-guppy-right.png");
-					}
-					else if ((!this->dirRight)&&((this->G)==3)){
-						this->setImage("small-guppy-left.png");
-					}
-					else if ((!this->dirRight)&&((this->G)==2)){
-						this->setImage("medium-guppy-left.png");
-					}
-					else if ((!this->dirRight)&&((this->G)==1)){
-						this->setImage("large-guppy-left.png");
-					}
-
-					//Ngejalanin ikannya, berdasarkan dir yang udah pasti bener:
-					double newX = this->getX() + 100*cos(this->getDirection())*delay; //speed bisa diatur2 lah ntar
-					double newY = this->getY() + 100*sin(this->getDirection())*delay;
-					if (this->getSpace()->moveTo(this->getId(), TYPE_GUPPY, newX, newY)) {
-						this->setX(newX);
-						this->setY(newY);
-
-					//Update atribut:
-						if ((this->lastFed)>=LAPAR){
-							setHunger(true);
+			if (food != NULL){
+				if (this->isAbleToConsume(*food)){
+					this->getSpace()->remove(food->getId(), TYPE_FOOD);
+					setLastFed(this->tickTime);
+					setHunger(false);
+					growthCounter++;
+				}else{	//kalau tidak bisa consume makanan
+					//ngejar food, belum implementasi (findNearestFood, setDir, setSpeed)
+					if (food != NULL){
+						setDirection(atan2(food->getY()-this->getY(), food->getX()-this->getX()));
+						if ((getDirection()>=2*atan(1))&&(getDirection()<=6*atan(1))){
+							setDirRight(true);
 						}
-					}else{
-						setDirection(getDirection()+2*atan(1));
-					}
+						else {
+							setDirRight(false);
+						}
 
+						if ((this->dirRight)&&((this->G)==1)){	//menentukan gambar ikan yang dipakai
+							this->setImage("small-guppy-left.png");
+						}
+						else if ((this->dirRight)&&((this->G)==2)){
+							this->setImage("medium-guppy-left.png");
+						}
+						else if ((this->dirRight)&&((this->G)==3)){
+							this->setImage("large-guppy-left.png");
+						}
+						else if ((!this->dirRight)&&((this->G)==1)){
+							this->setImage("small-guppy-right.png");
+						}
+						else if ((!this->dirRight)&&((this->G)==2)){
+							this->setImage("medium-guppy-right.png");
+						}
+						else if ((!this->dirRight)&&((this->G)==3)){
+							this->setImage("large-guppy-right.png");
+						}
+
+						//Ngejalanin ikannya, berdasarkan dir yang udah pasti bener:
+						double newX = this->getX() + 100*cos(this->getDirection())*delay; //speed bisa diatur2 lah ntar
+						double newY = this->getY() + 100*sin(this->getDirection())*delay;
+						if (this->getSpace()->moveTo(this->getId(), TYPE_GUPPY, newX, newY)) {
+							this->setX(newX);
+							this->setY(newY);
+
+						//Update atribut:
+							if ((this->tickTime - this->lastFed)>=LAPAR){
+								setHunger(true);
+							}
+						}else{
+							setDirection(getDirection()+2*atan(1));
+						}
+						cek = true;
+					}
 				}
+
+			
 
 			}
 		}
-	}else{*/
+	}
+
+	if (!cek){
+		cout << 5 << endl;
 		if (((this->tickTime)-(this->lastDrift))>=this->driftLength){
 			setDirection(fRand(0,8*atan(1)));	//randomize direction
 			driftLength = rand() % 200;	//maksimal 3 tick
@@ -242,45 +208,58 @@ void Guppy::tick(LinkedList<Food> &foods, LinkedList<Coin> &coins, double delay)
 			this->setY(newY);
 
 		//Update atribut:
-			if ((this->lastFed)>=LAPAR){
+			if ((this->tickTime - this->lastFed)>=LAPAR){
 				setHunger(true);
 			}
 		}else{
 			setDirection(getDirection()+2*atan(1));
 		}
-	//}
+	}
 	this->increaseTick();
 }
 
 Food* Guppy::findNearestFood(LinkedList<Food> &foods){
+	cout << "a" << endl;
 	Element<Food>* eFood = foods.getFirst();
-	
-	Food *nearestFood = eFood->getInfo();
-	double minDistance = distanceWithFood(
-		this->getX(),
-		this->getY(),
-		nearestFood->getX(), 
-		nearestFood->getY());
-
-	eFood = eFood->getNext();
-	while (eFood != NULL) {
-		double distance = distanceWithFood(
+	cout << "b" << endl;
+	if (eFood != NULL){
+		Food *nearestFood = eFood->getInfo();
+		double minDistance = distanceWithFood(
 			this->getX(),
 			this->getY(),
-			eFood->getInfo()->getX(), 
-			eFood->getInfo()->getY());
-		if (distance < minDistance) {
-			minDistance = distance;
-			nearestFood = eFood->getInfo();
-		}
-
+			nearestFood->getX(), 
+			nearestFood->getY());
+		cout << "c" << endl;
 		eFood = eFood->getNext();
-	}
+		while (eFood != NULL) {
+			cout << "d" << endl;
+			double distance = distanceWithFood(
+				this->getX(),
+				this->getY(),
+				eFood->getInfo()->getX(), 
+				eFood->getInfo()->getY());
+			if (distance < minDistance) {
+				minDistance = distance;
+				nearestFood = eFood->getInfo();
+				cout << "e" << endl;
+			}
 
-	return nearestFood;
+			eFood = eFood->getNext();
+		}
+		cout << "f" << endl;
+		return nearestFood;
+	}else{
+		return NULL;
+	}
 }
 
 bool Guppy::isAbleToConsume(Food const& food){ //belum diimplementasi
+	cout << "Food X: " << food.getX();
+	cout << "Food Y: " << food.getY();
+	cout << "Top: " << this->getTop();
+	cout << "Bottom: " << this->getBottom();
+	cout << "Right: " << this->getRight();
+	cout << "Left: " << this->getLeft();
 	return (this->getSpace())->isExist(food.getId(), TYPE_FOOD) && (
 		(food.getX() > this->getLeft()) && 
 		(food.getX() < this->getRight()) &&
